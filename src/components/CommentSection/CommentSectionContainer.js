4407adc0c66293aa6ef5c136cb317c6d1b883ffa
Andre Freeman <asf@mail.usf.edu>
2020-03-03 19:10:28 -0500

    Got comment section container working

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,11 +5,11 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
-  // Add state for the comments
-
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {props.comments.map(curVal => {
+        return <Comment comment = {curVal} />
+      })}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -4,6 +4,7 @@
 import React from "react";
 
 const LikeSection = props => {
+  console.log(props)
   return (
     <div>
     <div
